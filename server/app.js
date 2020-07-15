fb271980bdf51041af42c14bcf867399188730e0
Himanshu Gupta <guptahimanshuji@gmail.com>
2020-07-15 12:26:46 +0530

    initial

---
+++ server/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import bodyParser from "body-parser"
+import logger from 'morgan';
+
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+
+var app = express();
+
+app.use(bodyParser.json())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+export default app;
